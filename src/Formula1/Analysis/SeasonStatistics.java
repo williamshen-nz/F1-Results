67073f7b6fd0bddc496834682a9b27c07d769ea8
William Shen <william.w.y.shen@gmail.com>
2017-06-12 17:46:26 +1000

    Update statistics to include constructors championship

--- src/Formula1/Analysis/SeasonStatistics.java
+++ src/Formula1/Analysis/SeasonStatistics.java
@@ -6,7 +6,7 @@ import Helpers.JSON;
 import java.util.*;
 
 public class SeasonStatistics {
-    public static ArrayList<Position> getPoints(Season season) {
+    public static ArrayList<TeamPosition> getDriverPoints(Season season) {
         HashMap<Driver, Integer> points = new LinkedHashMap<>(20);
         for (Race race : season.getRaces()) {
             for (Result result : race.getSessions().getRace().getResults()) {
@@ -18,22 +18,75 @@ public class SeasonStatistics {
             }
         }
 
-        ArrayList<Position> rankings = new ArrayList<>(20);
+        ArrayList<TeamPosition> rankings = new ArrayList<>(20);
         for (Map.Entry<Driver, Integer> entry : points.entrySet()) {
-            rankings.add(new Position(entry.getKey(), entry.getValue()));
+            rankings.add(new TeamPosition(entry.getKey(), entry.getValue()));
             Collections.sort(rankings);
         }
         return rankings;
     }
 
-    private static class Position implements Comparable<Position> {
+    public static ArrayList<ConstructorPosition> getConstructorPoints(Season season) {
+        HashMap<Team, Integer> points = new LinkedHashMap<>(10);
+        for (Race race : season.getRaces()) {
+            for (Result result : race.getSessions().getRace().getResults()) {
+                Team team = result.getDriver().getTeam();
+                Integer currentPoints = points.get(team);
+                if (currentPoints == null)
+                    points.put(team, ((RaceResult) result).getPoints());
+                else
+                    points.put(team, currentPoints + ((RaceResult) result).getPoints());
+            }
+        }
+
+        ArrayList<ConstructorPosition> rankings = new ArrayList<>(20);
+        for (Map.Entry<Team, Integer> entry : points.entrySet()) {
+            rankings.add(new ConstructorPosition(entry.getKey(), entry.getValue()));
+            Collections.sort(rankings);
+        }
+        return rankings;
+    }
+
+
+    private static class ConstructorPosition implements Comparable<ConstructorPosition> {
+        private Team team;
+        private int points;
+
+        public ConstructorPosition() {
+        }
+
+        public ConstructorPosition(Team team, int points) {
+            this.team = team;
+            this.points = points;
+        }
+
+        public Team getTeam() {
+            return team;
+        }
+
+        public int getPoints() {
+            return points;
+        }
+
+        @Override
+        public int compareTo(ConstructorPosition o) {
+            return o.points - this.points;
+        }
+
+        @Override
+        public String toString() {
+            return JSON.stringify(this);
+        }
+    }
+
+    private static class TeamPosition implements Comparable<TeamPosition> {
         private Driver driver;
         private int points;
 
-        public Position() {
+        public TeamPosition() {
         }
 
-        public Position(Driver driver, int points) {
+        public TeamPosition(Driver driver, int points) {
             this.driver = driver;
             this.points = points;
         }
@@ -47,7 +100,7 @@ public class SeasonStatistics {
         }
 
         @Override
-        public int compareTo(Position o) {
+        public int compareTo(TeamPosition o) {
             return o.points - this.points;
         }
 
