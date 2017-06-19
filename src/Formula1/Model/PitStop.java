cf7dc21385359ef1a9193ae05d585ea8e6542cc6
William Shen <william.w.y.shen@gmail.com>
2017-06-19 22:51:14 +1200

    Add PitStop schema

---
+++ src/Formula1/Model/PitStop.java
@@ -0,0 +1,50 @@
+package Formula1.Model;
+
+import Helpers.JSON;
+
+public class PitStop {
+    private Driver driver;
+    private int stops;
+    private int lap;
+    private String timeOfDay;
+    private String time;
+    private String total;
+
+    public PitStop(Driver driver, int stops, int lap, String timeOfDay, String time, String total) {
+        this.driver = driver;
+        this.stops = stops;
+        this.lap = lap;
+        this.timeOfDay = timeOfDay;
+        this.time = time;
+        this.total = total;
+    }
+
+    public Driver getDriver() {
+        return driver;
+    }
+
+    public int getStops() {
+        return stops;
+    }
+
+    public int getLap() {
+        return lap;
+    }
+
+    public String getTimeOfDay() {
+        return timeOfDay;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public String getTotal() {
+        return total;
+    }
+
+    @Override
+    public String toString() {
+        return JSON.stringify(this);
+    }
+}
